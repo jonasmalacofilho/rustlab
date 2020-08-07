5c946609158c6ec1f27dd4fed25d98c479d1dc8d
Jonas Malaco <jonas@protocubo.io>
2020-08-07 08:24:44 -0300

    errors: disable models that aren't (yet at least) very useful

--- errors/src/lib.rs
+++ errors/src/lib.rs
@@ -1,4 +1,7 @@
+// Toy error models, at most suitable for simple CLI utilities
 pub mod box_of_dyn_error;
-pub mod newtype_string;
-pub mod newtype_string_error;
 pub mod string;
+
+// Not very useful error models
+// pub mod newtype_string;
+// pub mod newtype_string_error;
