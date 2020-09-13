e092ecfb4aedc457485f8b1604abce4e6ef07e47
Jonas Malaco <jonas@protocubo.io>
2020-09-13 00:46:48 -0300

    collections: hash_map: add fallible insert/get_or_insert
    With insights from:
        https://users.rust-lang.org/t/issue-with-hashmap-and-fallible-update/44960

--- collections/src/hash_map.rs
+++ collections/src/hash_map.rs
@@ -64,3 +64,36 @@ mod key_from_field {
         assert_eq!(Rc::strong_count(&key), 1); // only local key
     }
 }
+
+mod fallible_insert {
+    use super::*;
+
+    #[allow(dead_code)]
+    fn fallible(key: &str) -> Result<i32, &'static str> {
+        if key == "foo" {
+            Ok(42)
+        } else {
+            Err("forbidden key")
+        }
+    }
+
+    #[test]
+    fn foo() {
+        fn get_or_insert<'a>(map: &'a mut HashMap<String, i32>, key: String) -> Result<&'a i32, &'static str> {
+            use std::collections::hash_map::Entry::*;
+
+            Ok(match map.entry(key) {
+                Occupied(e) => e.into_mut(),
+                Vacant(e) => {
+                    let value = fallible(e.key())?;
+                    e.insert(value)
+                }
+            })
+        }
+
+        let mut map = HashMap::new();
+
+        assert_eq!(get_or_insert(&mut map, String::from("foo")), Ok(&42));
+        assert_eq!(get_or_insert(&mut map, String::from("bar")), Err("forbidden key"));
+    }
+}
