79169691c0a12bc3cbef4d0b8d0a5f7014e8f6ff
Jonas Malaco <jonas@protocubo.io>
2020-09-17 17:49:19 -0300

    hello_server2: reply with some contents from runtime file/template

--- hello_server2/src/main.rs
+++ hello_server2/src/main.rs
@@ -1,6 +1,7 @@
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
@@ -19,13 +20,16 @@ fn handle_connection(mut stream: TcpStream) {
     );
 
     let mut buf = [0; 4096];
-
     stream.read(&mut buf).unwrap();
 
     eprintln!("{}", String::from_utf8_lossy(&buf[..]));
 
-    let res = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read("hello.html").unwrap();
 
+    let res = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n", contents.len());
     stream.write(res.as_bytes()).unwrap();
+
+    stream.write(&contents).unwrap();
+
     stream.flush().unwrap();
 }
