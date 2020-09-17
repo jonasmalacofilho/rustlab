5e08e6276ed99142dd1d6e624b978a2c9bb8a137
Jonas Malaco <jonas@protocubo.io>
2020-09-17 17:41:47 -0300

    hello_server2: read some of the request and write a simple response

--- hello_server2/src/main.rs
+++ hello_server2/src/main.rs
@@ -1,4 +1,6 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
@@ -6,9 +8,24 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        eprintln!(
-            "Connection established from {}",
-            stream.peer_addr().unwrap()
-        );
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    eprintln!(
+        "Connection established with {}",
+        stream.peer_addr().unwrap()
+    );
+
+    let mut buf = [0; 4096];
+
+    stream.read(&mut buf).unwrap();
+
+    eprintln!("{}", String::from_utf8_lossy(&buf[..]));
+
+    let res = "HTTP/1.1 200 OK\r\n\r\n";
+
+    stream.write(res.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
