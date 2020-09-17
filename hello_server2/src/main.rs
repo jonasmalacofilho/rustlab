d5757330c284b1c826c0a86f169081bedec3e8fe
Jonas Malaco <jonas@protocubo.io>
2020-09-17 18:17:35 -0300

    hello_server2: add conditional hello/404 responses

--- hello_server2/src/main.rs
+++ hello_server2/src/main.rs
@@ -19,22 +19,29 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut reader = BufReader::new(stream.try_clone().unwrap());
 
-    eprintln!(
-        "Connection established with {}",
-        stream.peer_addr().unwrap()
-    );
+    let mut request_line = String::new();
+    reader.read_line(&mut request_line).unwrap();
 
-    let mut request = String::new();
-    reader.read_line(&mut request).unwrap();
+    let pretty_peer = match stream.peer_addr() {
+        Ok(a) => a.to_string(),
+        Err(_) => String::from("unknown peer"),
+    };
+    eprintln!("{} from {}", request_line.trim_end(), pretty_peer);
 
-    eprintln!("{}", request);
+    let (status_line, contents) = if request_line.starts_with("GET / HTTP") {
+        ("HTTP/1.1 200 OK\r\n", "hello.html")
+    } else {
+        ("HTTP/1.1 404 Not Found\r\n", "404.html")
+    };
 
-    let contents = fs::read("hello.html").unwrap();
+    let contents = fs::read(contents).unwrap();
 
-    let res = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n", contents.len());
-    stream.write(res.as_bytes()).unwrap();
+    stream.write(status_line.as_bytes()).unwrap();
 
+    stream.write(format!("Content-Length: {}\r\n\r\n", contents.len()).as_bytes()).unwrap();
     stream.write(&contents).unwrap();
 
     stream.flush().unwrap();
+
+    eprintln!("{}", status_line);
 }
