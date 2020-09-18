b6f6821bf1191db633fc6bf6f468781fa44bac22
Jonas Malaco <jonas@protocubo.io>
2020-09-17 23:45:34 -0300

    hello_server2: add minimal implementation of a thread pool

--- hello_server2/src/thread_pool.rs
+++ hello_server2/src/thread_pool.rs
@@ -1,21 +1,30 @@
-use std::thread;
-use std::thread::JoinHandle;
+use std::sync::mpsc::{self, Receiver, Sender};
+use std::sync::{Arc, Mutex};
+use std::thread::{self, JoinHandle};
+
+type Task = Box<dyn FnOnce() + Send + 'static>;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: Sender<Task>,
 }
 
 impl ThreadPool {
     pub fn new(size: u32) -> ThreadPool {
         assert!(size > 0);
 
-        let workers = (0..size).map(|id| Worker::new(id)).collect::<Vec<_>>();
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
 
-        ThreadPool { workers }
+        let workers = (0..size)
+            .map(|id| Worker::new(id, receiver.clone()))
+            .collect::<Vec<_>>();
+
+        ThreadPool { workers, sender }
     }
 
-    pub fn execute(&self, task: impl FnOnce() -> () + Send + 'static) {
-        thread::spawn(task);
+    pub fn execute(&self, task: impl FnOnce() + Send + 'static) {
+        self.sender.send(Box::new(task)).unwrap();
     }
 }
 
@@ -25,8 +34,12 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: u32) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new(id: u32, receiver: Arc<Mutex<Receiver<Task>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let task = receiver.lock().unwrap().recv().unwrap();
+
+            task();
+        });
 
         Worker { id, thread }
     }
