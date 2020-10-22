eb8c3d50dc14945c34570fcd0e6412116e8d9cdc
Jonas Malaco <jonas@protocubo.io>
2020-10-22 03:03:30 -0300

    zero2prod: simplify test setup with direct use of tokio

--- zero2prod/tests/health_check.rs
+++ zero2prod/tests/health_check.rs
@@ -2,23 +2,18 @@ use std::net::{SocketAddr, TcpListener};
 
 use zero2prod;
 
-fn setup_server() -> SocketAddr {
-    let listener = TcpListener::bind("127.0.0.1:0").unwrap();
-    let addr = listener.local_addr().unwrap();
+fn setup_server() -> Result<SocketAddr, std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0")?;
+    let addr = listener.local_addr()?;
 
-    // actix_rt::spawn requires a future with Output = ()
-    async fn run_server(listener: TcpListener) {
-        zero2prod::run(listener).unwrap().await.unwrap()
-    }
+    tokio::spawn(zero2prod::run(listener)?);
 
-    actix_rt::spawn(run_server(listener));
-
-    addr
+    Ok(addr)
 }
 
 #[actix_rt::test]
 async fn returns_200_with_empty_body() {
-    let server = setup_server();
+    let server = setup_server().unwrap();
 
     let client = reqwest::Client::new();
 
