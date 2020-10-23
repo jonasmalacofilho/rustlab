0f11f334aa2314e03f884f1e7d3eeede46ddf887
Jonas Malaco <jonas@protocubo.io>
2020-10-23 03:44:42 -0300

    zero2prod: parse x-www-form-urlencoded data for POST /subscribe

--- zero2prod/src/lib.rs
+++ zero2prod/src/lib.rs
@@ -1,14 +1,30 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer, Responder};
+use serde::Deserialize;
 use std::net::TcpListener;
+
+#[derive(Deserialize, Debug)]
+struct SubscriberData {
+    name: String,
+    email: String,
+}
 
 async fn health_check() -> impl Responder {
     ""
 }
 
+async fn subscribe(form: web::Form<SubscriberData>) -> impl Responder {
+    dbg!(form);
+    ""
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscribe", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }
--- zero2prod/tests/health_check.rs
+++ zero2prod/tests/health_check.rs
@@ -1,3 +1,5 @@
+use reqwest::multipart::Form;
+use reqwest::{Client, StatusCode};
 use std::net::{SocketAddr, TcpListener};
 
 use zero2prod;
@@ -15,7 +17,7 @@ fn setup_server() -> Result<SocketAddr, std::io::Error> {
 async fn returns_200_with_empty_body() {
     let server = setup_server().expect("test setup failed");
 
-    let client = reqwest::Client::new();
+    let client = Client::new();
 
     let response = client
         .get(&format!("http://{}/health_check", server))
@@ -25,4 +27,52 @@ async fn returns_200_with_empty_body() {
 
     assert!(response.status().is_success());
     assert_eq!(response.content_length(), Some(0));
+}
+
+#[actix_rt::test]
+async fn subscribes_valid_name_and_email() {
+    let server = setup_server().expect("test setup failed");
+
+    let client = Client::new();
+
+    let form = [("name", "Aria"), ("email", "to@aria.me")];
+
+    let response = client
+        .post(&format!("http://{}/subscribe", server))
+        .form(&form)
+        .send()
+        .await
+        .expect("request failed");
+
+    assert!(response.status().is_success());
+}
+
+#[actix_rt::test]
+async fn refuses_missing_name_or_email() {
+    let server = setup_server().expect("test setup failed");
+
+    let client = Client::new();
+
+    // TODO try 3 different test functions that all call the same helper
+    let test_cases = vec![
+        ([("name", "Aria")], "is missing email"),
+        ([("email", "to@aria.me")], "is missing name"),
+        ([("_", "__")], "is missing both name and email"),
+    ];
+
+    for (form, desc) in test_cases {
+        let response = client
+            .post(&format!("http://{}/subscribe", server))
+            .form(&form)
+            .send()
+            .await
+            .expect("request failed");
+
+        assert_eq!(
+            response.status(),
+            StatusCode::BAD_REQUEST,
+            "failed to return 400 error when form {}",
+            desc
+        );
+    }
 }
