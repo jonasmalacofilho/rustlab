ff2edd652ca07e39fcf7a4f3352548f0d2f4c1af
Jonas Malaco <jonas@protocubo.io>
2021-04-02 23:47:56 -0300

    prime-sieve: report time in a more useful manner

--- prime-sieve/src/main.rs
+++ prime-sieve/src/main.rs
@@ -1,11 +1,11 @@
 //! Measure how many sieves can be executed in a given period.
 
 use prime_sieve::Sieve;
-use std::time::Instant;
 use std::collections::HashMap;
+use std::time::Instant;
 
 fn main() {
-    let period = 5;
+    let period = 5.;
     let upper_limit: usize = 1_000_000;
 
     let prime_counts = [
@@ -17,23 +17,26 @@ fn main() {
         (1_000_000, 78_498),
         (10_000_000, 664_579),
         (100_000_000, 5_761_455),
-    ].iter().map(|x| *x).collect::<HashMap<usize, usize>>();
+    ]
+    .iter()
+    .copied()
+    .collect::<HashMap<usize, usize>>();
 
     let start = Instant::now();
-    let mut duration = 0;
+    let mut duration = 0.;
     let mut passes = 0;
 
     while duration < period {
         let sieve = Sieve::build(upper_limit);
         assert_eq!(sieve.count_primes(), prime_counts[&upper_limit]);
         passes += 1;
-        duration = Instant::now().duration_since(start).as_secs();
+        duration = Instant::now().duration_since(start).as_secs_f64();
     }
 
     println!(
-        "{} passes in {} seconds; each pass took {} seconds in average",
+        "{} passes in {:.1} seconds; on average, each pass took {:.2E} seconds",
         passes,
         duration,
-        duration as f32 / passes as f32
+        duration / passes as f64
     );
 }
