33d52f64679b37467230cdd3f23abdf59cb2318b
Jonas Malaco <jonas@protocubo.io>
2021-06-30 03:00:45 -0300

    randomness: use OsRng instead of CSPRNG seeded from it

--- randomness/src/main.rs
+++ randomness/src/main.rs
@@ -1,7 +1,7 @@
 use structopt::StructOpt;
 
 use rand::prelude::*;
-use rand::SeedableRng;
+use rand::rngs::OsRng;
 
 #[derive(Debug, StructOpt)]
 /// Generate some randomness
@@ -25,8 +25,7 @@ fn main() {
 }
 
 fn gen_digits(num: usize) -> Vec<u8> {
-    let mut rgn = rand_chacha::ChaCha20Rng::from_entropy();
-    (0..num).map(|_| rgn.gen_range(0..10)).collect()
+    (0..num).map(|_| OsRng.gen_range(0..10)).collect()
 }
 
 fn gen_silly_digits(num: usize) -> Vec<u8> {
@@ -35,12 +34,11 @@ fn gen_silly_digits(num: usize) -> Vec<u8> {
         "mathematically impossible to generate more than 8 silly digits"
     );
 
-    let mut rgn = rand_chacha::ChaCha20Rng::from_entropy();
     let mut digits: Vec<u8> = Vec::with_capacity(num);
 
     for i in 0..num {
         loop {
-            let digit = rgn.gen_range(0..10);
+            let digit = OsRng.gen_range(0..10);
             if i == 0 || (!digits.contains(&digit) && (digits[i - 1] as i8 - digit as i8).abs() > 1)
             {
                 digits.push(digit);
